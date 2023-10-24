3a61e405f633fbc59e6922f63b27497b13984faf
Kong Wei Hang <kwh3884858@gmail.com>
2023-10-24 23:43:53 +0800

    Add Sound Booster Switch

---
+++ Assets/Data/VoiceBooster/VoiceBooster.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class VoiceBooster : UdonSharpBehaviour
+{
+    public float m_boosterRadius = 5.0f;
+    //public SphereCollider m_boosterArea = null;
+    [Tooltip("This one need to set as player layer")]
+    public LayerMask m_areaLayerMask;
+    private VRCPlayerApi[] m_currentControlledPlayer;
+
+    void Start()
+    {
+        //if (m_boosterArea == null)
+        //{
+        //    return;
+        //}
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void Update()
+    {
+        //RaycastHit[] playerInBoosterArea = Physics.SphereCastAll(transform.position, m_boosterRadius, Vector3.up, Mathf.Infinity, m_areaLayerMask);
+        //foreach (var player in playerInBoosterArea)
+        //{
+        //    Debug.Log(player);
+        //}
+    }
+
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, m_boosterRadius);
+    }
+#endif
+}
